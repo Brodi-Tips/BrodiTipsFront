77d8d056b1dce47097b7e2b76cb0b81863d79401
C1322033 Julio Cesar Ferreira Lima <c1322033@interno.bb.com.br>
2023-01-08 19:31:53 -0300

    Pix page done.

---
+++ fifa/pix/index.js
@@ -0,0 +1,16 @@
+
+const onCopy30Days = () => {
+  const pix30Days = "00020101021126770014br.gov.bcb.pix0114422716900001630237Nao esqueca de adicionar seu telefone520400005303986540599.905802BR5915Viral Desenvolv6009FORTALEZA62130509FIFA30DAS630428EF"
+  navigator.clipboard.writeText(pix30Days);
+
+}
+
+const onCopy90Days = () => {
+  const pix90Days = "00020101021126770014br.gov.bcb.pix0114422716900001630237Nao esqueca de adicionar seu telefone5204000053039865406245.905802BR5915Viral Desenvolv6009FORTALEZA62140510FIFA90DIAS6304A13E"
+  navigator.clipboard.writeText(pix90Days);
+}
+
+const onCopyInfinite = () => {
+  const pixInfinite = "00020101021126770014br.gov.bcb.pix0114422716900001630237Nao esqueca de adicionar seu telefone52040000530398654071999.995802BR5915Viral Desenvolv6009FORTALEZA62150511FIFAPREMIUM63041284"
+  navigator.clipboard.writeText(pixInfinite);
+}
