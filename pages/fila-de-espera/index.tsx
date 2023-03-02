9782d4a6748ebb4d50fa1bd47ae6dfc3edef5ad8
C1322033 Julio Cesar Ferreira Lima <c1322033@interno.bb.com.br>
2023-03-01 21:21:19 -0300

    Add fila de espera.

---
+++ pages/fila-de-espera/index.tsx
@@ -0,0 +1,29 @@
+import styles from "styles/Home.module.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import Head from "next/head";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const url = "https://forms.gle/Ur4ZT2i8zCDDMVYs7";
+    setTimeout(() => router.push(String(url)), 2000);
+  }, [router]);
+
+  return (
+    <>
+      <Head>
+        <title>Bródi Tips</title>
+      </Head>
+      <img
+        className={styles.loading + " index"}
+        width={160}
+        height={160}
+        src="/images/logo.png"
+        alt="Bródi Tips"
+      />
+    </>
+  );
+}
